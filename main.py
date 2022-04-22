0bc777eb183c820d7326a13467aae56f80826df9
ilyashirko <82837921+ilyashirko@users.noreply.github.com>
2022-04-22 17:36:35 +0300

    made bot communicative

--- main.py
+++ main.py
@@ -1,43 +1,64 @@
-from environs import Env
-import requests
-import json
 import time
 
-USER_REVIEWS_URL = 'https://dvmn.org/api/user_reviews/'
+import requests
+import telegram
+
+from environs import Env
 
 LONG_POLLING_URL = 'https://dvmn.org/api/long_polling/'
-
 
 
 if __name__ == '__main__':
     env = Env()
     env.read_env()
 
-    headers = {
-        "Authorization": f"Token {env.str('token')}"
-    }
+    bot = telegram.Bot(token=env.str('TELEGRAM_TOKEN'))
+
+    headers = {"Authorization": f"Token {env.str('DEVMAN_TOKEN')}"}
     params = {}
+
     while True:
         try:
-            response = requests.get(LONG_POLLING_URL, headers=headers, params=params)
-            response_info = response.json()
-            params.update(
-                {
-                    "timestamp": int(response_info["timestamp_to_request"])
-                }
+            response = requests.get(
+                LONG_POLLING_URL,
+                headers=headers,
+                params=params
             )
-            print(json.dumps(response_info, indent=4, ensure_ascii=False))
-        except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError) as error:
+            response_info = response.json()
+            if response_info["status"] == 'found':
+                message = ''
+                for attempt in response_info["new_attempts"]:
+                    lesson_title = attempt['lesson_title']
+                    if attempt["is_negative"]:
+                        message += (
+                            f'The lesson "{attempt["lesson_title"]}" '
+                            f'has been sent for revision.\n\n'
+                            f'{attempt["lesson_url"]}'
+                        )
+                    else:
+                        message += (
+                            f'Congratulations! '
+                            f'Lesson "{attempt["lesson_title"]}" passed!'
+                        )
+                    bot.send_message(
+                        chat_id=env.str('USER_ID'),
+                        text=message
+                    )
+
+                params.update(
+                    {
+                        "timestamp": int(
+                            response_info["last_attempt_timestamp"] + 1
+                        )
+                    }
+                )
+            else:
+                params.update(
+                    {
+                        "timestamp": int(response_info["timestamp_to_request"])
+                    }
+                )
+        except (requests.exceptions.ReadTimeout,
+                requests.exceptions.ConnectionError) as error:
             print(f'{error}\nRepeate request...')
             time.sleep(5)
-        
-
-
-
-
-    """
-    'status' in ['timeout', 'found'],
-    'new_attempts: [
-        'is_negative': true / false
-    ]
-    """
